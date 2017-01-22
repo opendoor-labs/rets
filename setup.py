990e6d9c3c018e26dde21f90f2aa31df3a7a0d69
Martin Liu <martin@opendoor.com>
2017-01-21 18:39:37 -0800

    Fix missing comma in setup.py

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
+        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
     ],
