2e3931bdd170df06b52c0dc4e62b3cd1e39d4fe9
Martin Liu <martin@opendoor.com>
2017-01-21 17:31:17 -0800

    Remove requirements.txt and define requires in setup.py directly

--- rets/__init__.py
+++ rets/__init__.py
@@ -1,6 +1,11 @@
 from rets.client import RetsClient
 from rets.http.client import RetsHttpClient
 
+__title__ = 'rets'
+__version__ = '0.2.0'
+__author__ = 'Martin Liu <martin@opendoor.com>'
+__license__ = 'MIT License'
+
 __all__ = [
     'RetsClient',
     'RetsHttpClient',
--- setup.py
+++ setup.py
@@ -1,17 +1,35 @@
-from pip.download import PipSession
-from pip.req import parse_requirements
-from setuptools import setup, find_packages
+from setuptools import setup
 
-long_desc = 'Python 3.5 client for the Real Estate Transaction Standard (RETS) Version 1.7.2'
+long_desc = 'Python 3 client for the Real Estate Transaction Standard (RETS) Version 1.7.2'
 
-install_requires = [str(req.req) for req in parse_requirements('requirements.txt',
-                                                               session=PipSession())]
+install_requires = [
+    'beautifulsoup4>=4.5.1',
+    'flake8>=3.2.1',
+    'lxml>=3.6.4',
+    'requests>=2.12.3',
+    'requests-toolbelt>=0.7.0',
+    'udatetime>=0.0.11',
+]
+
+tests_requires = install_requires + [
+    'pytest>=3.0.5',
+]
+
+packages = [
+    'rets',
+    'rets.client',
+    'rets.http',
+    'rets.http.parsers',
+]
 
 setup(
     name='rets',
     version='0.1.0',
     description='rets',
     long_description=long_desc,
+    author='Martin Liu',
+    author_email='martin@opendoor.com',
+    url='https://github.com/opendoor-labs/rets',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
@@ -23,13 +41,15 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
     ],
-    author='Opendoor',
-    author_email='developers@opendoor.com',
-    url='https://github.com/opendoor-labs/rets',
+    license='MIT License',
     install_requires=install_requires,
-    packages=find_packages(),
+    tests_require=tests_requires,
+    packages=packages,
 )
