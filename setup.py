11fb7e0ed9eec80ae48c99cb21666936c061a4b7
Michael Chen <mike@opendoor.com>
2017-03-24 12:56:07 -0700

    Bump version: 0.3.2 → 0.3.2-dev

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ packages = [
 
 setup(
     name='rets-python',
-    version='0.3.2',
+    version='0.3.2-dev',
     description='rets-python',
     long_description=long_desc,
     author='Martin Liu',
