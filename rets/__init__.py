0857742c440f9f7ec539d7de67a7f22ea02a475e
Martin Liu <martin.xs.liu@gmail.com>
2018-07-01 18:10:54 -0700

    Bump version: 0.3.11 → 0.4.0

--- rets/__init__.py
+++ rets/__init__.py
@@ -3,7 +3,7 @@ from rets.http.client import RetsHttpClient
 from rets.http.data import Metadata, Object, SearchResult, SystemMetadata
 
 __title__ = 'rets'
-__version__ = '0.3.11'
+__version__ = '0.4.0'
 __author__ = 'Martin Liu <martin@opendoor.com>'
 __license__ = 'MIT License'
 
--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ packages = [
 
 setup(
     name='rets-python',
-    version='0.3.11',
+    version='0.4.0',
     description='rets-python',
     long_description=long_desc,
     author='Martin Liu',
