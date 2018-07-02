870d9020f917cab0e6ac1c10d9a2c4a757f828cd
Martin Liu <martin.xs.liu@gmail.com>
2018-07-02 13:22:45 -0700

    Bump version: 0.4.0 → 0.4.1

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ packages = [
 
 setup(
     name='rets-python',
-    version='0.4.0',
+    version='0.4.1',
     description='rets-python',
     long_description=long_desc,
     author='Martin Liu',
