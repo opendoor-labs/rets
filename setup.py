72a39bd70275586b9d118d700f382fc102dda8c1
mingujo <mingu08@berkeley.edu>
2018-10-09 17:52:29 -0700

    Bump version: 0.4.1 → 0.4.2

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ packages = [
 
 setup(
     name='rets-python',
-    version='0.4.1',
+    version='0.4.2',
     description='rets-python',
     long_description=long_desc,
     author='Martin Liu',
