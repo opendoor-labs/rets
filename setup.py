007704e9ac120e65fb552e7ac7068c392d027985
Geobio Boo <geobio.boo@opendoor.com>
2021-05-24 15:06:37 -0700

    Bump version: 0.4.6 → 0.4.7

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ packages = [
 
 setup(
     name='rets-python',
-    version='0.4.6',
+    version='0.4.7',
     description='rets-python',
     long_description=long_desc,
     author='Martin Liu',
