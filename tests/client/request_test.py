338dfdaa2e03d3011e535a9c78332fe7c865621c
Geobio Boo <geobio.boo@opendoor.com>
2021-06-07 16:33:48 -0700

    dont pass arg = true. use default value instead

--- tests/client/request_test.py
+++ tests/client/request_test.py
@@ -12,7 +12,6 @@ def test_rets_ua_authorization_false():
                             username='user',
                             password='pass',
                             send_rets_ua_authorization=send_auth,
-                            use_get_method=False,
                             )
     client._session = MagicMock()
     client._http_request(url='test.url')
@@ -23,19 +22,14 @@ def test_rets_ua_authorization_false():
     assert 'RETS-UA-Authorization' not in client._session.post.call_args_list[0][1]['headers']
 
 
-def test_rets_ua_authorization_true():
-    send_auth = True
-
+def test_rets_ua_authorization_default():
+    # by default, sends 'RETS-UA-Authorization'
     client = RetsHttpClient(login_url='test.url',
                             username='user',
                             password='pass',
-                            send_rets_ua_authorization=send_auth,
-                            use_get_method=False,
                             )
     client._session = MagicMock()
     client._http_request(url='test.url')
-
-    assert client._send_rets_ua_authorization == send_auth
 
     assert client._session.post.called
     assert 'RETS-UA-Authorization' in client._session.post.call_args_list[0][1]['headers']
